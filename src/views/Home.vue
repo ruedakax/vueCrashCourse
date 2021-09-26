6770f953f6e13a61d1ff7d437c61dd916bf757eb
ruedakax <ruedakax@gmail.com>
2021-09-26 18:07:01 -0500

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,81 @@
+<template>    
+    <AddTask v-show="showAddTask" @add-task="addTask"/>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
+</template>
+<script>
+import Tasks from '../components/Tasks.vue'
+import AddTask from '../components/AddTask.vue'
+
+export default {
+    name:'Home',
+    components: {
+        Tasks,
+        AddTask,
+    },
+    props:{
+        showAddTask: Boolean,
+    },
+    data(){
+        return {
+            tasks:[],            
+        }
+    },
+    methods:{
+        toggleAddTask(){
+            this.showAddTask = !this.showAddTask
+        },
+        async addTask(task){
+        //console.log(task)
+            const res = await fetch('/api/tasks',{
+                method:'POST',
+                headers:{
+                'Content-type':'application/json',          
+                },
+                body: JSON.stringify(task),
+            })
+
+        const data  = await res.json()
+            this.tasks = [...this.tasks, data]
+        },
+        async deleteTask(id){
+            if(confirm('¿Está seguro?')){
+                const res = await fetch(`api/tasks/${id}`,{
+                method:'DELETE',
+
+                })
+                res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)):alert('Error borrando tarea')
+                
+            }
+        },
+        async toggleReminder(id){
+            const taskToToggle  = await this.fetchTask(id)
+            const updTask = {...taskToToggle,reminder:!taskToToggle.reminder}
+            const res = await fetch(`api/tasks/${id}`,{
+                method:'PUT',
+                headers:{
+                    'Content-type':'application/json',          
+                },
+                body: JSON.stringify(updTask),
+            })
+            const data  = await res.json()
+
+            this.tasks = this.tasks.map((task) => task.id===id?{...task, reminder: data.reminder}:task)      
+        },
+        async fetchTasks(){
+            const res = await fetch('api/tasks')
+            const data = await res.json()            
+            return data
+        },
+        async fetchTask(id){
+            const res = await fetch(`api/tasks/${id}`)
+            const data = await res.json()
+            return data
+        },
+        
+    },
+    /*lifecycle Method*/
+    async created(){
+            this.tasks = await this.fetchTasks()
+    },
+}
+</script>
